4d33d029056d0cd40409c97b8fadc40b4c871eb0
Jun <asdf7894k@gmail.com>
2025-06-02 11:36:32 +0800

    Update scripts.js

--- TemplateData/scripts/scripts.js
+++ TemplateData/scripts/scripts.js
@@ -204,8 +204,8 @@ function adjustMobileCanvasSize() {
 function applyCanvasDimensions(width, height) {
 
   if (canvas.isMobileDevice) {
-    canvas.width = width * 3.3;
-    canvas.height = height * 3.3;
+    canvas.width = width * 3.5;
+    canvas.height = height * 3.5;
   }
 
   // Apply CSS dimensions
