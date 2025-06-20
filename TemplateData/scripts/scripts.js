e71eb8a2b1adb7c5d0738036916142034106a6a4
Jun <asdf7894k@gmail.com>
2025-06-20 18:17:26 +0800

    update version v1.0.0

--- TemplateData/scripts/scripts.js
+++ TemplateData/scripts/scripts.js
@@ -459,7 +459,7 @@ function loadUnityGame() {
     streamingAssetsUrl: "StreamingAssets",
     companyName: "chfn",
     productName: "soundimals",
-    productVersion: "0.2.3",
+    productVersion: "1.0.0",
     showBanner: unityShowBanner,
     // Performance optimizations
     printErr: function(message) {
